463f1d6da82d10e42b113534e67535f7cf8d8703
duarteIpleiria <2222123@my.ipleiria.pt>
2026-01-14 22:29:14 +0000

    fix

--- academics/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package pt.ipleiria.estg.dei.ei.dae.academics;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
